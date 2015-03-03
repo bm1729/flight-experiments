0b42e9f9afc0c235ea29e60651afc5fd16100362
bm1729 <ben.mumford1729@gmail.com>
2015-03-03 08:48:38 +0000

    Working data component test

--- test/client/spec/components/dataComponent.spec.js
+++ test/client/spec/components/dataComponent.spec.js
@@ -1,10 +1,22 @@
-/* globals describeComponent, it, expect */
+/* globals describeComponent, it, expect, spyOnEvent, beforeEach, setupComponent */
 
 describeComponent('component/dataComponent', function() {
     
     "use strict";
+    
+    beforeEach(function() {
+        this.setupComponent();
+    });
 
-    it('simple test', function() {
-        expect(true).toBe(true);
+    it('uiItemsRequested', function() {
+        spyOnEvent(document, 'dataItemsServed');
+        this.component.trigger('uiItemsRequested');
+        expect('dataItemsServed').toHaveBeenTriggeredOn(document);
+    });
+
+    it('uiItemRequested', function() {
+        spyOnEvent(document, 'dataItemServed');
+        this.component.trigger('uiItemRequested', {id: 1});
+        expect('dataItemServed').toHaveBeenTriggeredOn(document);
     });
 });
